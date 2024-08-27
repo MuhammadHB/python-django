9081cdcff0b62e493e01bce45c0fe93a5642f042
Muhammad Bunyarit <141912757+MuhammadHB@users.noreply.github.com>
2024-08-28 04:21:39 +0700

    done visitor count and visitor info cookies
    done visitor count and visitor info cookies

---
+++ djangopro/test_geoip.py
@@ -0,0 +1,14 @@
+import geoip2.database
+
+db_path = 'C:/Users/M/Downloads/GeoLite2-City_20240823/GeoLite2-City_20240823/GeoLite2-City.mmdb'
+
+def test_geoip(ip_address):
+    try:
+        with geoip2.database.Reader(db_path) as reader:
+            response = reader.city(ip_address)
+            print("City:", response.city.name)
+            print("Country:", response.country.name)
+    except Exception as e:
+        print("Error:", e)
+
+test_geoip('185.212.169.106')  # استخدم عنوان IP معروف للتجربة
